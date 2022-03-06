d49153ab7c21643d063de440edb72eff30e42dfb
Victor Aarão <victoraarao16@gmail.com>
2022-03-06 19:47:17 -0300

    Gerador de senhas

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let Password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghiklmnopqrstuvwxyzA B C DEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%&"
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){ 
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+}
+
+containerPassword.classList.remove("hide");
+ Password.innerHTML = pass;
+ novaSenha = pass;
+
+}
+
+function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+}
+
+//  4DkBmOH
+// QRNpdQVWx3TOr7dBWpfKsCua5
